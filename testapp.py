88d79d8fba24704cbb91f7cb0979d723a708f5a4
Eric French <ericfrench2015@gmail.com>
2023-03-15 07:45:25 -0500

    adding test page for streamlit

---
+++ testapp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 07:33:35 2022
+
+@author: ericf
+"""
+
+import streamlit as st
+
+
+st.set_page_config(page_title="My page",page_icon=":tada:", layout="wide")
+
+st.title("Testing basic streamlit capabilities and deployment")
+
+st.write("This page will be built out using youtube tutorials")
